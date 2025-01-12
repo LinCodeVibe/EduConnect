2c12a799c350d4d30f0aadcdc6ea497b3d8be880
zahra <zahra.shaikh@sjsu.edu>
2025-01-12 11:20:05 -0800

    adding generate study plan to layout

--- educonnect-master/src/pages/Dashboard.js
+++ educonnect-master/src/pages/Dashboard.js
@@ -1,14 +1,3 @@
-// import React from "react";
-// import Layout from "../components/Layout";
-
-// export const Dashboard = () => {
-//   return (
-//     <Layout>
-//       <h1>This is the dashboard page!</h1>
-//     </Layout>
-//   );
-// };
-
 import React from "react";
 import Layout from "../components/Layout";
 import { useHistory } from "react-router-dom"; // Import useHistory for navigation
