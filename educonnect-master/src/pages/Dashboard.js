ad67babc450b611c35a31268fabb2306a95568eb
zahra <zahra.shaikh@sjsu.edu>
2025-01-13 18:00:27 -0800

    adding delete card functionality and displaying message if no card is created yet

--- educonnect-master/src/pages/Dashboard.js
+++ educonnect-master/src/pages/Dashboard.js
@@ -18,6 +18,7 @@ import {
   query,
   where,
   getDocs,
+  deleteDoc,
 } from "firebase/firestore";
 
 export const Dashboard = () => {
@@ -71,6 +72,28 @@ export const Dashboard = () => {
     }
   }, [isAuthenticated, db]);
 
+  const handleDelete = async (id) => {
+    if (auth.currentUser) {
+      const userUid = auth.currentUser.uid;
+
+      try {
+        const studyPlanRef = doc(
+          collection(doc(db, "users", userUid), "studyPlans"),
+          id
+        );
+
+        await deleteDoc(studyPlanRef);
+
+        setStudyPlans((prevPlans) =>
+          prevPlans.filter((plan) => plan.id !== id)
+        );
+        console.log("Study plan deleted successfully!");
+      } catch (error) {
+        console.error("Error deleting study plan: ", error);
+      }
+    }
+  };
+
   const goToGenerateStudyPlan = () => {
     history.push("/generate");
   };
@@ -85,12 +108,43 @@ export const Dashboard = () => {
         style={{ display: "flex", justifyContent: "center", padding: "20px" }}
       >
         <div style={{ textAlign: "left", maxWidth: "1000px", width: "100%" }}>
-          <h1>
+          <h1 style={{ marginBottom: "30px" }}>
             Welcome to EduConnect. Let's tackle your goals, one step at a time.
             You've got this!
           </h1>
 
-          <h3>In Progress:</h3>
+          {studyPlans.length === 0 && (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                marginTop: "30px",
+                padding: "20px",
+                backgroundColor: "#f9f9f9",
+                borderRadius: "10px",
+                marginBottom: "30px",
+              }}
+            >
+              <Typography
+                variant="h5"
+                sx={{
+                  fontSize: "1.8rem",
+                  color: "#4caf50",
+                  marginBottom: "10px",
+                }}
+              >
+                Looks like your study plans are on break!
+              </Typography>
+              <Typography variant="body1" sx={{ fontSize: "1.2rem" }}>
+                <strong>
+                  Hit that "+" button to get a head start on your study goals!
+                </strong>
+              </Typography>
+            </div>
+          )}
+
+          {studyPlans.length > 0 && <h3>Your current study plans:</h3>}
 
           {/* Grid layout for the cards */}
           <Grid container spacing={3}>
@@ -126,7 +180,9 @@ export const Dashboard = () => {
                   <CardActions>
                     <Button size="small">View</Button>
                     <Button size="small">Mark as Complete</Button>
-                    <Button size="small">Delete</Button>
+                    <Button size="small" onClick={() => handleDelete(plan.id)}>
+                      Delete
+                    </Button>
                   </CardActions>
                 </Card>
               </Grid>
@@ -142,11 +198,27 @@ export const Dashboard = () => {
                   alignItems: "center",
                   height: "100%",
                   cursor: "pointer",
+                  textAlign: "center",
                 }}
                 onClick={goToGenerateStudyPlan}
               >
                 <CardContent>
-                  <AddCircleIcon sx={{ fontSize: "4rem", color: "#4caf50" }} />
+                  <AddCircleIcon
+                    sx={{
+                      fontSize: "4rem",
+                      color: "#4caf50",
+                    }}
+                  />
+                  {studyPlans.length > 0 && (
+                    <Typography
+                      variant="h5"
+                      component="div"
+                      sx={{ fontSize: "1.5rem" }}
+                      marginTop="10px"
+                    >
+                      One more doesn't hurt, does it?
+                    </Typography>
+                  )}
                 </CardContent>
               </Card>
             </Grid>
